7a08f4bbec19df443af2c182c71e156621f66724
zhengke <zhengke996@163.com>
2025-02-26 10:27:35 +0800

    fix: 修改文件名大小写

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -78,11 +78,13 @@ onMounted(() => {
     <slot />
   </article>
   <div v-if="route.path !== '/'" class="prose m-auto mt-8 mb-8">
-    <router-link
+    <!-- <router-link
       :to="route.path.split('/').slice(0, -1).join('/') || '/'"
       class="font-mono no-underline opacity-50 hover:opacity-75"
-    >
-      cd ..
-    </router-link>
+    > -->
+    <a @click="router.back()" class="font-mono no-underline opacity-50 hover:opacity-75">    cd ..</a>
+  
+    <!-- </router-link> -->
+    <!--   :to="route.path.split('/').slice(0, -1).join('/') || '/'" -->
   </div>
 </template>
